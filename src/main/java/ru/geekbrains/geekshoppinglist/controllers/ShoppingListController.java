315c6e3a1a07b12b15abca4d07751ccc60c188c5
IZ-ui <Amtel34W>
2020-06-07 19:23:22 +0300

    initial commit

--- src/main/java/ru/geekbrains/geekshoppinglist/controllers/ShoppingListController.java
+++ src/main/java/ru/geekbrains/geekshoppinglist/controllers/ShoppingListController.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.geekshoppinglist.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,27 +11,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.geekbrains.geekshoppinglist.persist.ShoppingItem;
 import ru.geekbrains.geekshoppinglist.persist.ShoppingItemRepository;
+import ru.geekbrains.geekshoppinglist.persist.User;
+import ru.geekbrains.geekshoppinglist.persist.UserRepository;
+
+import java.security.Principal;
 
 @Controller
 public class ShoppingListController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingListController.class);
+
     private ShoppingItemRepository repository;
 
+    private final UserRepository userRepository;
+
     @Autowired
-    public ShoppingListController(ShoppingItemRepository repository) {
+    public ShoppingListController(ShoppingItemRepository repository, UserRepository userRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
-    public String indexPage(Model model) {
-        model.addAttribute("items", repository.findAll());
+    public String indexPage(Model model, Principal principal) {
+        logger.info("User name: {}", principal.getName());
+
+        model.addAttribute("items", repository.findByUserUsername(principal.getName()));
         model.addAttribute("item", new ShoppingItem());
         return "index";
     }
 
     @PostMapping
-    public String newShoppingItem(ShoppingItem shoppingItem) {
-        repository.save(shoppingItem);
+    public String newShoppingItem(ShoppingItem item, Principal principal) {
+        logger.info("User name: {}", principal.getName());
+
+        User user = userRepository.findByUsername(principal.getName()).get();
+        item.setUser(user);
+        repository.save(item);
         return "redirect:/";
     }
 

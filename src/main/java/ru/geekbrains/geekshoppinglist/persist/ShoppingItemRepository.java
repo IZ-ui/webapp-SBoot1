cc9addb25e367b544a010effdd7a5a937d0bc005
IZ-ui <Amtel34W>
2020-06-07 10:51:04 +0300

    initial commit

---
+++ src/main/java/ru/geekbrains/geekshoppinglist/persist/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.geekshoppinglist.persist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+}
